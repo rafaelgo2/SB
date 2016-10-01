7b8af232ec063c130d9b53c64bd1678e5de674ef
DCC Hackthon - Time C - Usuario 1 <hackthon11@dcc.ufmg.br>
2016-10-01 14:42:59 -0300

    R

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
-int main(){
+void fillFunction(map<string, int> &function){
+	function["nome"] = opCode;
+}
 
+int main(int argc, char *argv[]){
+	map<string, int> label, function;
+	fstream fin(argv[1]), fout(argv[2]);
+	int line = 0;	
+	string s;
+	while (fin >> s){
+		if (s[0] == '_'){
+			label[s] = line;
+			fin >> s;		
+		}
+		
+	}
 	return 0;
 }
