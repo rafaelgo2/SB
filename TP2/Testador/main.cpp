e297f9adda815d6c1cbafa0c741ab81ce3fb5e12
Luiz0tavio <luizotrv@gmail.com>
2016-11-06 22:57:49 -0200

    test comments

--- TP2/Testador/main.cpp
+++ TP2/Testador/main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <string>
 
+/* get platform */
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
     static const std::string PLATFORM="WIN";
 #else
@@ -11,9 +12,14 @@
 
 int main(int argc, char *argv[]){
 
+    /* executes the makefile from Montador and Linkador folders*/
     system("make -C ../Montador all");
     system("make -C ../Linkador all");
 
+    /* so, the makefile will generates the executable file
+     * (that depends of platform:)*/
+
+    /* copy the executables and past on test folder  */
     if(PLATFORM == "WIN"){
         system("cp ../Montador/montador.exe montador.exe");
         system("cp ../Linkador/linkador.exe linkador.exe");
@@ -22,7 +28,9 @@ int main(int argc, char *argv[]){
         system("cp ../Linkador/linkador linkador");
     }
 
+    /* Mount */
 	for (int i = 1; i < argc-1; i++){
+        /* print some steps on terminal */
 		printf("Montando %d!\n", i);
 		char montar[255];
         if(PLATFORM == "WIN"){
@@ -30,13 +38,16 @@ int main(int argc, char *argv[]){
         } else {
             sprintf(montar, "./montador %s saidaTmp%d.obj", argv[i], i);
         }
+        /* run mount */
 		system(montar);
 		printf("%s\n", montar);
 	}
 
+    /* Link */
 	char args[9999] = "";
 	for (int i = 1; i < argc-1; i++){
-		printf("Linkando %d!\n", i);
+        /* print some steps on terminal */
+        printf("Linkando %d!\n", i);
 		char link[999];
 		sprintf(link, "saidaTmp%d.obj ", i);
 		strcat(args, link);
@@ -48,9 +59,10 @@ int main(int argc, char *argv[]){
         sprintf(linkar, "./linkador %s %s", argv[argc-1], args);
     }
 	printf("%s\n", linkar);
-
+    /* run linker */
 	system(linkar);
 
+    /* clear */
 	for (int i = 1; i < argc-1; i++){
 		char remove[999];
 		sprintf(remove, "rm saidaTmp%d.obj", i);
