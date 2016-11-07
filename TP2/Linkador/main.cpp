0cfbec78aca6884fa1606149dcead69bb50ac272
Luiz0tavio <luizotrv@gmail.com>
2016-11-06 22:04:15 -0200

    main comments

--- TP2/Linkador/main.cpp
+++ TP2/Linkador/main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+/*
+ * Main function from Linker lib
+ *
+ * Get args file names and calls the main functions to make the link
+ * (more funcs info, see Linker Header file)
+ * */
 int main(int argc, char *argv[]){
 	Linker linker = Linker(argc, argv);
 	linker.initializeData();
